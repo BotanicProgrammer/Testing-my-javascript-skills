7c8482fc186d2f1981363c30302a929b31bced50
DevGM1 <84422216+DevGM1@users.noreply.github.com>
2021-06-04 23:22:40 -0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("I am just testing out my JavaScript skills.");
+console.log(" ");
+console.log("I will probably need to learn JS, Python, and C++ in order to make a game. I know HTML though.");
+console.log(" ");
+console.log("I've heard that C++ is known for its difficulty. Hopefully I can get the hang of it.");
